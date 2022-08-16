34c731980659d136a803527164050ffa6d652952
bipin-01 <poudel7700@gmail.com>
2022-08-16 23:53:49 +0545

    Added some examples on map

--- 01-array-string-manipulation/2_asm.js
+++ 01-array-string-manipulation/2_asm.js
@@ -28,3 +28,15 @@
 
   const taskNames = tasks.map(task => task.name)
   console.log(taskNames)
+
+
+// getting confused with the output need to work on this later
+//   const newArray = taskNames.map(function callback(task, index, array) {
+//     return array
+//   })
+
+// console.log(newArray)
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2)
+console.log(doubled)

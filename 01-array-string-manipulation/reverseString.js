45e60322e04323a5136878087d0dd97a1c64e9ff
bipin-01 <poudel7700@gmail.com>
2022-09-14 23:24:51 +0545

    Reverse String

---
+++ 01-array-string-manipulation/reverseString.js
@@ -0,0 +1,23 @@
+// write a function to reverse a given string
+// for e.g. if you are given 'Bipin Is a Dev'
+// The output should be 'veD a sI nipiB'
+
+const reverseString = (str)  => {
+
+    //check Input
+
+    if(!str || str.length < 2 ||  typeof str !== str ) {
+        return 'Enter valid input'
+    }
+
+    const backWardArr = [];
+    const strLength = str.length;
+
+    for(  let i = strLength - 1; i>=0; i--) {
+        backWardArr.push(str[i]);
+    }
+
+    return backWardArr.join('');
+}
+
+console.log(reverseString(undefined));
